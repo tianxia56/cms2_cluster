c434d79257ec25ed972508bb08b50361e1418d04
tianxia56 <tian.xia.tx56@yale.edu>
2024-12-28 23:54:56 -0500

    Update main_sbatch_array.sh

--- main_sbatch_array.sh
+++ main_sbatch_array.sh
@@ -13,7 +13,7 @@ mkdir -p output
 record_runtime() {
     job_name=$1
     start_time=$2
-    end_time=$(date +%s)
+    end_time=$3
     runtime=$((end_time - start_time))
     runtime_formatted=$(printf '%02d:%02d:%02d' $((runtime/3600)) $((runtime%3600/60)) $((runtime%60)))
     echo "Job name: $job_name, runtime: $runtime_formatted" >> output/totalruntime.txt
@@ -42,41 +42,41 @@ job_ids=()
 start_time=$(date +%s)
 job_id=$(sbatch --parsable run_cosi2_even_sel.sh)
 job_ids+=($job_id)
-record_runtime "run_cosi2_even_sel.sh" $start_time
+record_runtime "run_cosi2_even_sel.sh" $start_time $(date +%s)
 
 start_time=$(date +%s)
 job_id=$(sbatch --parsable run_cosi2_odd_sel.sh)
 job_ids+=($job_id)
-record_runtime "run_cosi2_odd_sel.sh" $start_time
+record_runtime "run_cosi2_odd_sel.sh" $start_time $(date +%s)
 
 # Submit a single task for neutral simulations
 start_time=$(date +%s)
 job_id=$(sbatch --parsable run_cosi2_neut.sh)
 job_ids+=($job_id)
-record_runtime "run_cosi2_neut.sh" $start_time
+record_runtime "run_cosi2_neut.sh" $start_time $(date +%s)
 
 # Submit the one pop stats task (ihs, nsl, ihh12) for both sel and neut separately, optimize runtime
 start_time=$(date +%s)
 job_id=$(sbatch --parsable run_ihs_sel.sh)
 job_ids+=($job_id)
-record_runtime "run_ihs_sel.sh" $start_time
+record_runtime "run_ihs_sel.sh" $start_time $(date +%s)
 
 start_time=$(date +%s)
 job_id=$(sbatch --parsable run_ihs_neut.sh)
 job_ids+=($job_id)
-record_runtime "run_ihs_neut.sh" $start_time
+record_runtime "run_ihs_neut.sh" $start_time $(date +%s)
 
 # Combine nsl and ihh12 into one job
 start_time=$(date +%s)
 job_id=$(sbatch --parsable run_nsl_ihh12.sh)
 job_ids+=($job_id)
-record_runtime "run_nsl_ihh12.sh" $start_time
+record_runtime "run_nsl_ihh12.sh" $start_time $(date +%s)
 
 # Submit the run_isafe.sh task to run in parallel (isafe, DAF) for selected sims
 start_time=$(date +%s)
 job_id=$(sbatch --parsable run_isafe.sh)
 job_ids+=($job_id)
-record_runtime "run_isafe.sh" $start_time
+record_runtime "run_isafe.sh" $start_time $(date +%s)
 
 # Extract population IDs from the original par file
 config_file="config.json"
@@ -84,11 +84,11 @@ demographic_model=$(python3 -c 'import json; print(json.load(open("'"$config_fil
 simulation_serial_number=$(python3 -c 'import json; print(json.load(open("'"$config_file"'"))["simulation_serial_number"])')
 pop_ids=($(grep "^pop_define" $demographic_model | awk '{print $2}'))
 
-# Submit fst deldaf.sh task before xpehh tasks
+# Submit fst deldaf.sh task
 start_time=$(date +%s)
 job_id=$(sbatch --parsable --export=ALL,pop_ids="${pop_ids[*]}" run_fst_deldaf.sh)
 job_ids+=($job_id)
-record_runtime "run_fst_deldaf.sh" $start_time
+record_runtime "run_fst_deldaf.sh" $start_time $(date +%s)
 
 # Pass the pairwise population IDs for two pop stats tasks (xpehh for both sel and neut)
 for ((i=0; i<${#pop_ids[@]}; i++)); do
@@ -99,13 +99,13 @@ for ((i=0; i<${#pop_ids[@]}; i++)); do
         start_time=$(date +%s)
         job_id=$(sbatch --parsable --export=ALL,pop1=$pop1,pop2=$pop2 run_xpehh_sel.sh)
         job_ids+=($job_id)
-        record_runtime "run_xpehh_sel.sh ($pop1 vs $pop2)" $start_time
+        record_runtime "run_xpehh_sel.sh ($pop1 vs $pop2)" $start_time $(date +%s)
 
         # Submit pairwise tasks for xpehh neut
         start_time=$(date +%s)
         job_id=$(sbatch --parsable --export=ALL,pop1=$pop1,pop2=$pop2 run_xpehh_neut.sh)
         job_ids+=($job_id)
-        record_runtime "run_xpehh_neut.sh ($pop1 vs $pop2)" $start_time
+        record_runtime "run_xpehh_neut.sh ($pop1 vs $pop2)" $start_time $(date +%s)
     done
 done
 
@@ -115,7 +115,7 @@ wait_for_jobs "${job_ids[@]}"
 # Submit the final task to normalize, collate stats and format results
 start_time=$(date +%s)
 final_job_id=$(sbatch --parsable make_norm_file.sh)
-record_runtime "make_norm_file.sh" $start_time
+record_runtime "make_norm_file.sh" $start_time $(date +%s)
 echo "Final job submitted with ID: $final_job_id"
 
 # Wait for make_norm_file.sh to complete
@@ -128,7 +128,7 @@ norm_jobs+=($(sbatch --parsable norm_ihs.sh))
 norm_jobs+=($(sbatch --parsable norm_nsl.sh))
 norm_jobs+=($(sbatch --parsable norm_ihh12.sh))
 norm_jobs+=($(sbatch --parsable norm_delihh.sh))
-record_runtime "norm_one_pop_stats.sh" $start_time
+record_runtime "norm_one_pop_stats.sh" $start_time $(date +%s)
 
 # Submit norm_xpehh.sh for each pair_id
 for ((i=0; i<${#pop_ids[@]}; i++)); do
@@ -138,7 +138,7 @@ for ((i=0; i<${#pop_ids[@]}; i++)); do
         pair_id=${pop1}_vs_${pop2}
         start_time=$(date +%s)
         norm_jobs+=($(sbatch --parsable norm_xpehh.sh $pair_id))
-        record_runtime "norm_xpehh.sh ($pair_id)" $start_time
+        record_runtime "norm_xpehh.sh ($pair_id)" $start_time $(date +%s)
     done
 done
 
@@ -148,7 +148,7 @@ wait_for_jobs "${norm_jobs[@]}"
 # Submit final_output.sh after all normalization jobs are done
 start_time=$(date +%s)
 final_output_job_id=$(sbatch --parsable final_output.sh)
-record_runtime "final_output.sh" $start_time
+record_runtime "final_output.sh" $start_time $(date +%s)
 echo "final_output.sh job submitted with ID: $final_output_job_id"
 
 # Record the total runtime
