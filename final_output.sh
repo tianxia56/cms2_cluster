f8ef4d3b7b3bf17a01e663a179aeb4110f941f29
tianxia56 <tian.xia.tx56@yale.edu>
2025-01-01 10:12:03 -0500

    Update final_output.sh

--- final_output.sh
+++ final_output.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=ycga
-#SBATCH --time=2:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
@@ -23,17 +23,7 @@ for ((sim_id=0; sim_id<=selected_simulation_number; sim_id++)); do
             Rscript make_mean_xpehh.R $sim_id $pair_id
         done
     done
-    Rscript collate_stats.R $sim_id $demographic_model $simulation_serial_number
 done
 
-# Zip all the output files into one single tsv zip with the current date in the name
-output_dir="output"
-current_date=$(date +%Y-%m-%d)
-zipfile="${output_dir}/${demographic_model}_batch${simulation_serial_number}_cms_stats_all_${current_date}.zip"
-zip -j $zipfile ${output_dir}/${demographic_model}_batch${simulation_serial_number}_cms_stats_*.tsv
-
-# Remove the individual tsv files after making the zip
-rm ${output_dir}/${demographic_model}_batch${simulation_serial_number}_cms_stats_*.tsv
-
-# Run clean.sh after all jobs are done
-# bash clean.sh
+# Call make-output.R
+Rscript make-output.R
